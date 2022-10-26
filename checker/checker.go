1d7c7a97032d6882f95fb43d4ac472ffd001bb8e
Matthew McPherrin <mattm@letsencrypt.org>
2022-10-26 17:24:02 -0400

    Switch to using boulder's diff function

--- checker/checker.go
+++ checker/checker.go
@@ -1,15 +1,14 @@
 package checker
 
 import (
-	"bytes"
 	"context"
-	"crypto/x509"
 	"fmt"
 	"log"
 	"math/big"
-	"sort"
 	"time"
 
+	"github.com/letsencrypt/boulder/crl/checker"
+	"github.com/letsencrypt/boulder/crl/crl_x509"
 	"github.com/letsencrypt/crl-monitor/db"
 	"github.com/letsencrypt/crl-monitor/storage"
 )
@@ -24,79 +23,8 @@ type Checker struct {
 }
 
 func fetchNotAfter(serial *big.Int) time.Time {
-	fmt.Printf("TODO: fetch cert with serial %s", serial)
-	return time.Now()
-}
-
-// CRLLint
-// TODO: stub function
-func CRLLint(crl *x509.RevocationList) error {
-	return nil
-}
-
-// Diff returns the sets of serials that were added and removed between two
-// CRLs. In order to be comparable, the CRLs must come from the same issuer, and
-// be given in the correct order (the "old" CRL's Number and ThisUpdate must
-// both precede the "new" CRL's).
-func Diff(old, new *x509.RevocationList) ([]*big.Int, error) {
-	if !bytes.Equal(old.AuthorityKeyId, new.AuthorityKeyId) {
-		return nil, fmt.Errorf("CRLs were not issued by same issuer")
-	}
-
-	if !old.ThisUpdate.Before(new.ThisUpdate) {
-		return nil, fmt.Errorf("old CRL %s does not precede new CRL %s", old.ThisUpdate, new.ThisUpdate)
-	}
-
-	if old.Number.Cmp(new.Number) > 0 {
-		return nil, fmt.Errorf("old CRL %d does not precede new CRL %d (%d)", old.Number, new.Number, old.Number.Cmp(new.Number))
-	}
-
-	// Sort both sets of serials so we can march through them in order.
-	oldSerials := make([]*big.Int, 0, len(old.RevokedCertificates))
-	for _, rc := range old.RevokedCertificates {
-		oldSerials = append(oldSerials, rc.SerialNumber)
-	}
-	sort.Slice(oldSerials, func(i, j int) bool {
-		return oldSerials[i].Cmp(oldSerials[j]) < 0
-	})
-
-	newSerials := make([]*big.Int, 0, len(new.RevokedCertificates))
-	for _, rc := range new.RevokedCertificates {
-		newSerials = append(newSerials, rc.SerialNumber)
-	}
-	sort.Slice(newSerials, func(i, j int) bool {
-		return newSerials[i].Cmp(newSerials[j]) < 0
-	})
-
-	// Work our way through both lists of sorted serials. If the old list skips
-	// past a serial seen in the new list, then that serial was added. If the new
-	// list skips past a serial seen in the old list, then it was removed.
-	i, j := 0, 0
-	added := make([]*big.Int, 0)
-	removed := make([]*big.Int, 0)
-	for {
-		if i >= len(oldSerials) {
-			added = append(added, newSerials[j:]...)
-			break
-		}
-		if j >= len(newSerials) {
-			removed = append(removed, oldSerials[i:]...)
-			break
-		}
-		cmp := oldSerials[i].Cmp(newSerials[j])
-		if cmp < 0 {
-			removed = append(removed, oldSerials[i])
-			i++
-		} else if cmp > 0 {
-			added = append(added, newSerials[j])
-			j++
-		} else {
-			i++
-			j++
-		}
-	}
-
-	return removed, nil
+	log.Printf("TODO: fetch cert with serial %s", serial)
+	return time.Now().Add(-24 * time.Hour) // TODO: everything expired in the past!
 }
 
 func (c *Checker) Check(ctx context.Context, bucket, object string) error {
@@ -117,23 +45,25 @@ func (c *Checker) Check(ctx context.Context, bucket, object string) error {
 		return err
 	}
 
-	crl, err := x509.ParseRevocationList(crlDER)
+	crl, err := crl_x509.ParseRevocationList(crlDER)
 	if err != nil {
 		return fmt.Errorf("error parsing current crl: %v", err)
 	}
-	prev, err := x509.ParseRevocationList(prevDER)
+	prev, err := crl_x509.ParseRevocationList(prevDER)
 	if err != nil {
 		return fmt.Errorf("error parsing previous crl: %v", err)
 	}
 
 	log.Printf("loaded CRL %d (len %d) and previous %d (len %d)", crl.Number, len(crl.RevokedCertificates), prev.Number, len(crl.RevokedCertificates))
 
-	err = CRLLint(crl)
-	if err != nil {
-		return fmt.Errorf("crl failed linting: %v", err)
-	}
+	//var issuer *issuance.Certificate // TODO
+	//ageLimit := 24 * time.Hour
+	//err = checker.Validate(crl, issuer, ageLimit)
+	//if err != nil {
+	//	return fmt.Errorf("crl failed linting: %v", err)
+	//}
 
-	err = c.lookForEarlyRemoval(crl, prev)
+	err = c.lookForEarlyRemoval(prev, crl)
 	if err != nil {
 		return err
 	}
@@ -141,28 +71,29 @@ func (c *Checker) Check(ctx context.Context, bucket, object string) error {
 	return c.lookForSeenCerts(ctx, crl)
 }
 
-func (c *Checker) lookForEarlyRemoval(crl *x509.RevocationList, prev *x509.RevocationList) error {
-	diff, err := Diff(prev, crl)
+func (c *Checker) lookForEarlyRemoval(prev *crl_x509.RevocationList, crl *crl_x509.RevocationList) error {
+	diff, err := checker.Diff(prev, crl)
 	if err != nil {
 		return err
 	}
 
-	log.Printf("checking for early CRL removal on %d serials\n", len(diff))
+	log.Printf("checking for early CRL removal on %d serials\n", len(diff.Removed))
 
-	for _, removed := range diff {
+	for _, removed := range diff.Removed {
 		notAfter := fetchNotAfter(removed)
 
 		if prev.ThisUpdate.Before(notAfter) {
 			// This certificate expired after the previous CRL was issued
 			// All removed CRLs should have been expired in the previous CRL
-			return fmt.Errorf("early removal of %v from crl %v", removed, prev)
+			return fmt.Errorf("early removal of %v from crl %v: previous CRL at %v is before cert notAfter %v",
+				removed, prev.Number, prev.ThisUpdate, notAfter)
 		}
 	}
 
 	return nil
 }
 
-func (c *Checker) lookForSeenCerts(ctx context.Context, crl *x509.RevocationList) error {
+func (c *Checker) lookForSeenCerts(ctx context.Context, crl *crl_x509.RevocationList) error {
 	monitoring, err := c.db.GetAllCerts(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to read from db: %v", err)
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.10.0
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.17.1
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.11
+	github.com/letsencrypt/boulder v0.0.0-20221024182155-868214b85eaf
 	github.com/mholt/acmez v1.0.4
 	github.com/stretchr/testify v1.8.0
 )
@@ -31,19 +32,49 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.6 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.16.19 // indirect
 	github.com/aws/smithy-go v1.13.3 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
+	github.com/facebookgo/limitgroup v0.0.0-20150612190941-6abd8d71ec01 // indirect
+	github.com/facebookgo/muster v0.0.0-20150708232844-fd3d7953fd52 // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/certificate-transparency-go v1.0.22-0.20181127102053-c25855a82c75 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/honeycombio/beeline-go v1.10.0 // indirect
+	github.com/honeycombio/libhoney-go v1.16.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/kr/pretty v0.3.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/jmhodges/clock v0.0.0-20160418191101-880ee4c33548 // indirect
+	github.com/klauspost/compress v1.15.7 // indirect
+	github.com/letsencrypt/pkcs11key/v4 v4.0.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/miekg/pkcs11 v1.1.1 // indirect
+	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.13.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.1 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	github.com/weppos/publicsuffix-go v0.20.1-0.20221016123633-0ad56ba8bf2e // indirect
+	github.com/zmap/zcrypto v0.0.0-20220402174210-599ec18ecbac // indirect
+	github.com/zmap/zlint/v3 v3.4.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
-	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
-	golang.org/x/text v0.3.7 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/net v0.0.0-20220926192436-02166a98028e // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
+	golang.org/x/text v0.3.8 // indirect
+	google.golang.org/grpc v1.49.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/alexcesaro/statsd.v2 v2.0.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
