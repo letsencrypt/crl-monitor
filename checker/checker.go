77537bde271cd05dcc094bdd63d4f25d502e9933
Matthew McPherrin <mattm@letsencrypt.org>
2022-10-27 14:01:14 -0400

    Remove early removal checks
    This code is in another PR, but we're not going down this path right now

--- checker/checker.go
+++ checker/checker.go
@@ -2,13 +2,8 @@ package checker
 
 import (
 	"context"
-	"crypto/x509"
-	"encoding/pem"
 	"fmt"
-	"io"
 	"log"
-	"math/big"
-	"net/http"
 	"time"
 
 	"github.com/letsencrypt/boulder/crl/checker"
@@ -19,58 +14,12 @@ import (
 )
 
 func New(database *db.Database, storage *storage.Storage) Checker {
-	// TODO: parameterize this
-	baseURL := "https://acme-v02.api.letsencrypt.org/acme/cert/"
-	fetcher := boulderAPIFetcher{baseURL: baseURL}
-	return Checker{db: database, storage: storage, fetcher: &fetcher}
+	return Checker{db: database, storage: storage}
 }
 
 type Checker struct {
 	db      *db.Database
 	storage *storage.Storage
-	fetcher ExpFetcher
-}
-
-type ExpFetcher interface {
-	FetchNotAfter(ctx context.Context, serial *big.Int) (time.Time, error)
-}
-
-type boulderAPIFetcher struct {
-	baseURL string
-}
-
-func (baf *boulderAPIFetcher) FetchNotAfter(ctx context.Context, serial *big.Int) (time.Time, error) {
-	// boulder implements non-acme-standard support for unauthenticated GETs of certificates
-	url := fmt.Sprintf("%s/%x", baf.baseURL, serial)
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	log.Printf("fetching serial for %d", serial)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return time.Time{}, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return time.Time{}, fmt.Errorf("error fetching certificate with serial %d: http status %d", serial, resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	block, _ := pem.Decode(body)
-	cert, err := x509.ParseCertificate(block.Bytes)
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	return cert.NotAfter, nil
 }
 
 func (c *Checker) Check(ctx context.Context, issuer *issuance.Certificate, bucket, object string) error {
@@ -105,42 +54,11 @@ func (c *Checker) Check(ctx context.Context, issuer *issuance.Certificate, bucke
 	ageLimit := 24 * time.Hour
 	err = checker.Validate(crl, issuer, ageLimit)
 	if err != nil {
-		log.Printf("crl failed linting: %v", err)
-		// TODO return fmt.Errorf
+		return fmt.Errorf("crl failed linting: %v", err)
 	}
 	log.Printf("crl %d successfully linted", crl.Number)
 
-	err = lookForEarlyRemoval(ctx, c.fetcher, prev, crl)
-	if err != nil {
-		return err
-	}
-
 	return c.lookForSeenCerts(ctx, crl)
-}
-
-func lookForEarlyRemoval(ctx context.Context, fetcher ExpFetcher, prev *crl_x509.RevocationList, crl *crl_x509.RevocationList) error {
-	diff, err := checker.Diff(prev, crl)
-	if err != nil {
-		return err
-	}
-
-	log.Printf("checking for early CRL removal on %d serials\n", len(diff.Removed))
-
-	for _, removed := range diff.Removed {
-		notAfter, err := fetcher.FetchNotAfter(ctx, removed)
-		if err != nil {
-			return err
-		}
-
-		if prev.ThisUpdate.Before(notAfter) {
-			// This certificate expired after the previous CRL was issued
-			// All removed CRLs should have been expired in the previous CRL
-			return fmt.Errorf("early removal of %v from crl %v: previous CRL at %v is before cert notAfter %v",
-				removed, prev.Number, prev.ThisUpdate, notAfter)
-		}
-	}
-
-	return nil
 }
 
 func (c *Checker) lookForSeenCerts(ctx context.Context, crl *crl_x509.RevocationList) error {
