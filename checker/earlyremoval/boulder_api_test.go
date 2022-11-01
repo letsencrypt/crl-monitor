ebdbab3b449b7a7ab1fa35fe7f4c330a1c49f04b
Matthew McPherrin <mattm@letsencrypt.org>
2022-10-31 21:55:20 -0400

    Make integration test into table test

--- checker/earlyremoval/boulder_api_test.go
+++ checker/earlyremoval/boulder_api_test.go
@@ -4,6 +4,7 @@ package earlyremoval
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 	"net/http"
 	"testing"
@@ -14,30 +15,34 @@ import (
 
 // This test hits the real boulder staging API!
 // It is behind the integration flag as a result.
-func TestBoulderStagingAPI(t *testing.T) {
-	baseURL := "https://acme-staging-v02.api.letsencrypt.org/get/cert"
-	baf := BoulderAPIFetcher{Client: http.DefaultClient, BaseURL: baseURL}
+func TestBoulderAPI(t *testing.T) {
+	// TODO: These integration tests hardcode certificates that we fetch from Boulder's API
+	// They may not stay available in boulder's API forever.
+	for _, tc := range []struct {
+		subdomain string
+		serial    string
+		expected  time.Time
+	}{
+		{
+			subdomain: "acme-staging-v02",
+			serial:    "fad21382e31f8218bc798416cea008da6940",
+			expected:  time.Date(2023, 01, 24, 12, 49, 02, 00, time.UTC),
+		},
+		{
+			subdomain: "acme-v02",
+			serial:    "04bc17a64a2c415af9ba4df32b73bf4e08e7",
+			expected:  time.Date(2022, 12, 05, 17, 35, 50, 00, time.UTC),
+		},
+	} {
+		t.Run(tc.subdomain, func(t *testing.T) {
+			baseURL := fmt.Sprintf("https://%s.api.letsencrypt.org/get/cert", tc.subdomain)
+			baf := BoulderAPIFetcher{Client: http.DefaultClient, BaseURL: baseURL}
 
-	// TODO:  This is a hardcoded serial and expiry combo.
-	// It may not stay available in boulder's API forever
-	serial := new(big.Int)
-	serial.SetString("fad21382e31f8218bc798416cea008da6940", 16)
-	expected := time.Date(2023, 01, 24, 12, 49, 02, 00, time.UTC)
-	notAfter, err := baf.FetchNotAfter(context.Background(), serial)
-	require.NoError(t, err)
-	require.Equal(t, expected, notAfter)
-}
-
-func TestBoulderProductionAPI(t *testing.T) {
-	baseURL := "https://acme-v02.api.letsencrypt.org/get/cert"
-	baf := BoulderAPIFetcher{Client: http.DefaultClient, BaseURL: baseURL}
-
-	// TODO:  This is a hardcoded serial and expiry combo.
-	// It may not stay available in boulder's API forever
-	serial := new(big.Int)
-	serial.SetString("04bc17a64a2c415af9ba4df32b73bf4e08e7", 16)
-	expected := time.Date(2022, 12, 05, 17, 35, 50, 00, time.UTC)
-	notAfter, err := baf.FetchNotAfter(context.Background(), serial)
-	require.NoError(t, err)
-	require.Equal(t, expected, notAfter)
+			serial := new(big.Int)
+			serial.SetString(tc.serial, 16)
+			notAfter, err := baf.FetchNotAfter(context.Background(), serial)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, notAfter)
+		})
+	}
 }
