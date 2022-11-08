7e09cd7a4e4687d34fa12f22bf716ed5c35bc4a4
Matthew McPherrin <mattm@letsencrypt.org>
2022-11-08 17:48:47 -0500

    Remove broken code from merge

--- lambda/churner/churner.go
+++ lambda/churner/churner.go
@@ -2,23 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
-
-	"github.com/letsencrypt/crl-monitor/churner"
 )
 
 func HandleRequest(ctx context.Context) error {
-	c, err := churner.New("localhost")
-	if err != nil {
-		return fmt.Errorf("error in setup: %w", err)
-	}
-
-	err = c.Churn(ctx)
-	if err != nil {
-		return fmt.Errorf("error in churning: %w", err)
-	}
 	return nil
 }
 
