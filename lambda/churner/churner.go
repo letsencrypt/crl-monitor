6efb8beb7779f6463042b11ef3925c68ddb2577d
Aaron Gable <aaron@aarongable.com>
2026-01-13 17:46:31 -0800

    Add documentation and reasoning to churner.go

--- lambda/churner/churner.go
+++ lambda/churner/churner.go
@@ -11,8 +11,14 @@ import (
 	"github.com/letsencrypt/crl-monitor/churner"
 )
 
+// HandleRequest returns lambda handler responsible for both of Churner's
+// tasks: issuing and revoking certificates, and checking to ensure that
+// no certificates in the database are too old (i.e. that they've shown up
+// on at least one CRL and been removed from the db by the Checker).
 func HandleRequest(c *churner.Churner) func(context.Context) error {
 	return func(ctx context.Context) error {
+		// Part 1: Issue a certificate, immediately revoke it, and
+		// insert a database entry indicating when it was issued and revoked.
 		err := c.RegisterAccount(ctx)
 		if err != nil {
 			return fmt.Errorf("registering acme account: %w", err)
@@ -23,6 +29,10 @@ func HandleRequest(c *churner.Churner) func(context.Context) error {
 			return fmt.Errorf("churning: %w", err)
 		}
 
+		// Part 2: Load all certificates from the database (most likely
+		// including the one that was just issued), and check to make sure
+		// that any certs found there are recent enough that we're not
+		// worried about them not having appeared on a CRL yet.
 		missing, err := c.CheckMissing(ctx)
 		if err != nil {
 			return fmt.Errorf("checking for missing certs: %w", err)
